427a48a41900a09c4789d04e0c7a79a2b5a008c6
firefly-sylestia <khanquadir19@gmail.com>
2026-06-11 14:54:42 +0530

    Fix icon size modifier import

--- app/src/main/java/com/marvelspectrum/shared/presentation/components/icons/RhythmIcon.kt
+++ app/src/main/java/com/marvelspectrum/shared/presentation/components/icons/RhythmIcon.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Typeface
 import android.os.Build
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
